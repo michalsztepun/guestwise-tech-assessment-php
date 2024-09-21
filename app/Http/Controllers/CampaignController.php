7c5ebdfa8613c0d8b68c5455d2ae3d517df0bb4e
Michał Sztepun <michal.sztepun@sourcetoad.com>
2024-09-21 21:49:45 +0200

    feat: enable caching for campaign controller index method

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -9,6 +9,7 @@ use App\Models\Brand;
 use App\Models\Campaign;
 use Carbon\CarbonImmutable;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Cache;
 
 class  CampaignController extends Controller
 {
@@ -23,19 +24,23 @@ class  CampaignController extends Controller
         $sortDirection = $request->order ?? 'asc';
         $orderBy = $this->getOrderByField($request->sort);
 
-        $campaigns = Campaign::select('campaigns.name', 'brand_id')
-            ->with(['brand' => function ($query) {
-                $query->select('id', 'name');
-            }])
-            ->withCount([
-                'impressions' => fn($query) => $query->whereBetween('occurred_at', [$startDate, $endDate]),
-                'interactions' => fn($query) => $query->whereBetween('occurred_at', [$startDate, $endDate]),
-                'conversions' => fn($query) => $query->whereBetween('occurred_at', [$startDate, $endDate]),
-            ])
-            ->when($request->brand, fn($query) => $query->where('brand_id', $request->brand))
-            ->join('brands', 'campaigns.brand_id', '=', 'brands.id')
-            ->orderByRaw("{$orderBy} {$sortDirection}")
-            ->paginate();
+        $cacheKey = "campaigns_index_{$startDate->format('Ymd')}_{$endDate->format('Ymd')}_{$orderBy}_{$sortDirection}_{$request->page}_{$request->brand}";
+        $campaigns = Cache::remember($cacheKey, 600, function() use ($startDate, $endDate, $request, $orderBy, $sortDirection) {
+            return Campaign::select('campaigns.name', 'brand_id')
+                ->with(['brand' => function ($query) {
+                    $query->select('id', 'name');
+                }])
+                ->withCount([
+                    'impressions' => fn($query) => $query->whereBetween('occurred_at', [$startDate, $endDate]),
+                    'interactions' => fn($query) => $query->whereBetween('occurred_at', [$startDate, $endDate]),
+                    'conversions' => fn($query) => $query->whereBetween('occurred_at', [$startDate, $endDate]),
+                ])
+                ->when($request->brand, fn($query) => $query->where('brand_id', $request->brand))
+                ->whereBetween('campaigns.created_at', [$startDate, $endDate])
+                ->join('brands', 'campaigns.brand_id', '=', 'brands.id')
+                ->orderByRaw("{$orderBy} {$sortDirection}")
+                ->paginate();
+        });
 
         return view('campaigns.index', [
             'brands' => Brand::orderBy('name', 'asc')->get(),
